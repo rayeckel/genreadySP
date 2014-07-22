122e269a6bd68d11d911e04942cf7a81df4daa68
Ray Eckel <rayeckel@yahoo.com>
2014-07-22 13:50:10 -0500

    Finalized inheritance bugs

---
+++ GRSPClassLibrary/Pages/AccessTokenPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.SharePoint.Client;
+using System.Web;
+using System.Web.UI;
+
+namespace GRSPClassLibrary.Pages
+{
+    public class AccessTokenPage : Page
+    {
+        protected string accessToken = "";
+
+        protected override void OnLoad(EventArgs e)
+        {
+            this.getSessionAccessToken();
+
+            base.OnLoad(e);
+        }
+
+        private void getSessionAccessToken()
+        {
+            string contextTokenString = TokenHelper.GetContextTokenFromRequest(Request);
+
+            if (contextTokenString != null)
+            {
+                SharePointContextToken contextToken = TokenHelper.ReadAndValidateContextToken(contextTokenString, Request.Url.Authority);
+                var sharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+                this.accessToken = TokenHelper.GetAccessToken(contextToken, sharepointUrl.Authority).AccessToken;              
+            }
+            else if (!IsPostBack)
+            {
+                Response.Write("Could not find a context token.");
+            }
+        }
+    }
+}
---
+++ GRSPClassLibrary/Web/ClientAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.SharePoint.Client;
+
+namespace GRSPClassLibrary.Web
+{
+    public class ClientAccess
+    {
+        private string SITE_URL = "https://generationreadydev.sharepoint.com/sites/re_/logtest";
+        private string sessionAccessToken;
+
+        public ClientAccess(string sessionAccessToken)
+        {
+            this.sessionAccessToken = sessionAccessToken;
+        }
+
+        protected ClientContext GetClientAccessContextWithToken()
+        {
+            return TokenHelper.GetClientContextWithAccessToken(SITE_URL, sessionAccessToken);
+        }
+    }
+}
