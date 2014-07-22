8ddefe98f7f230759aae13c7f31dec15b964d49c
Ray Eckel <rayeckel@yahoo.com>
2014-07-22 09:34:01 -0500

    genready sp projects main repo init

---
+++ SystemLogs/SystemLogsWeb/testApp1/Default.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.SharePoint.Client;
+using Microsoft.IdentityModel.S2S.Tokens;
+using System.Net;
+using System.IO;
+using System.Xml;
+using SystemLogs.LogWriter;
+
+namespace SystemLogs.Pages
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getAccessToken();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Button1_ClickHandler();
+        }
+
+        private void getAccessToken()
+        {
+            string contextTokenString = TokenHelper.GetContextTokenFromRequest(Request);
+
+            if (contextTokenString != null)
+            {
+                SharePointContextToken contextToken = TokenHelper.ReadAndValidateContextToken(contextTokenString, Request.Url.Authority);
+                var sharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+                string accessToken = TokenHelper.GetAccessToken(contextToken, sharepointUrl.Authority).AccessToken;
+                Button1.CommandArgument = accessToken;
+            }
+            else if (!IsPostBack)
+            {
+                Response.Write("Could not find a context token.");
+                return;
+            }
+        }
+
+        private void Button1_ClickHandler()
+        {
+            string fName = TextBox1.Text;
+            string lname = TextBox2.Text;
+            string favColor = DropDownList1.SelectedValue;
+            string luckyNum = DropDownList2.SelectedValue;
+
+            string title = "User input Log";
+            string Description = fName + " " + lname + " Favorite Color: " + favColor + "  Lucky Number: " + luckyNum;
+
+            string accessToken = Button1.CommandArgument;
+
+            var logWriter = new Log(accessToken);
+            logWriter.WriteLog(title, Description);
+
+            Label1.Text = Description;
+        }
+    }
+}
---
+++ SystemLogs/SystemLogsWeb/testApp1/Log.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.SharePoint.Client;
+
+namespace SystemLogs.LogWriter
+{
+    public class Log
+    {
+        private string LOG_TITLE = "System Log";
+        private string SITE_URL = "https://generationreadydev.sharepoint.com/sites/re_/logtest";
+        private string accessToken;
+
+        public Log(string sessionAccessToken)
+        {
+            accessToken = sessionAccessToken;
+        }
+
+        public void WriteLog(string Title, string Description)
+        {
+            using (var clientContext = TokenHelper.GetClientContextWithAccessToken(SITE_URL, accessToken))
+            {
+                ListItem SystemLogListItem = GenerateSystemLogListItem(clientContext);
+
+                SystemLogListItem["Title"] = Title;
+                SystemLogListItem["Description"] = Description;
+
+                SystemLogListItem.Update();
+
+                clientContext.ExecuteQuery();
+            }
+        }
+
+        public void WriteLog(string Title, string Description, string Detail)
+        {
+            using (var clientContext = TokenHelper.GetClientContextWithAccessToken(SITE_URL, accessToken))
+            {
+                ListItem SystemLogListItem = GenerateSystemLogListItem(clientContext);
+
+                SystemLogListItem["Title"] = Title;
+                SystemLogListItem["Description"] = Description;
+                SystemLogListItem["Detail"] = Detail;
+
+                SystemLogListItem.Update();
+
+                clientContext.ExecuteQuery();
+            }
+        }
+
+        private ListItem GenerateSystemLogListItem(ClientContext clientContext)
+        {
+            Web oWebsite = clientContext.Web;
+            clientContext.Load(oWebsite);
+
+            List SystemLogList = oWebsite.Lists.GetByTitle(LOG_TITLE);
+
+            var ListItemCreateInfo = new ListItemCreationInformation();
+            ListItem SystemLogListItem = SystemLogList.AddItem(ListItemCreateInfo);
+
+            return SystemLogListItem;
+        }
+    }
+}
