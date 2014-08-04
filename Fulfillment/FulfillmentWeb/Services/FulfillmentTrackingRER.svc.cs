a0ec7e8e47d0166c8a810b352e0296d6fd3333ae
Ray Eckel <rayeckel@yahoo.com>
2014-08-04 11:37:25 -0500

    Added logic to reverse changes in the case of a report edit

--- Fulfillment/FulfillmentWeb/Services/FulfillmentTrackingRER.svc.cs
+++ Fulfillment/FulfillmentWeb/Services/FulfillmentTrackingRER.svc.cs
@@ -14,13 +14,10 @@ namespace FulfillmentWeb.Services
         protected override void ExecuteRER(SPRemoteEventProperties properties, ClientContext clientContext)
         {
             clientContext.Load(clientContext.Web, web => web.Lists);
-            clientContext.ExecuteQuery();
+            clientContext.ExecuteQuery();     
 
-            //Guid listId = properties.ItemEventProperties.ListId;
-            //int Id = properties.ItemEventProperties.ListItemId;        
-
-            updateAllocationsListITem(clientContext, properties);
-            updateArticlesListITem(clientContext, properties);
+            updateAllocationsListItem(clientContext, properties);
+            updateArticlesListItem(clientContext, properties);
 
             switch (properties.EventType)
             {
@@ -68,51 +65,82 @@ namespace FulfillmentWeb.Services
                         break;
                     }
             }
-
-            //clientContext.ExecuteQuery();
         }
 
-        private void updateAllocationsListITem(ClientContext clientContext, SPRemoteEventProperties properties)
+        private void updateAllocationsListItem(ClientContext clientContext, SPRemoteEventProperties properties)
+        {
+            var allocationId = Convert.ToString(properties.ItemEventProperties.AfterProperties[Constants.LIST_ITEM_ALLOCATION_ID]);
+            var units = Convert.ToDecimal(properties.ItemEventProperties.AfterProperties[Constants.INPUT_UNIT]);
+
+            var allocationListItem = getAllocationsListItem(properties, clientContext, allocationId);
+            incrementAllocationsFulfilled(allocationListItem, units);
+
+            //If the allocation ID is changing, reverse the updates to the old allocation.
+            var oldAllocationId = Convert.ToString(properties.ItemEventProperties.AfterProperties[Constants.INPUT_PREVIOUS_ALLOCATION_ID]);
+            if (allocationId != oldAllocationId)
+            {
+                var oldAllocationListItem = getAllocationsListItem(properties, clientContext, oldAllocationId);
+                decrementAllocationsFulfilled(oldAllocationListItem, units);
+
+                //Decrement the article so that when 'updateArticleListItem' is called,
+                //It does not result in too much being added.
+                var articleId = Convert.ToString(properties.ItemEventProperties.AfterProperties[Constants.LIST_ITEM_ARTICLE_ID]);
+                var oldArticlesListItem = getArticlesListItem(properties, clientContext, articleId);
+                decrementArticlesFulfilled(oldArticlesListItem, units);
+            }
+
+            clientContext.ExecuteQuery();
+        }
+
+        private void updateArticlesListItem(ClientContext clientContext, SPRemoteEventProperties properties)
+        {
+            var articleId = Convert.ToString(properties.ItemEventProperties.AfterProperties[Constants.LIST_ITEM_ARTICLE_ID]);
+            var units = Convert.ToDecimal(properties.ItemEventProperties.AfterProperties[Constants.INPUT_UNIT]);
+
+            var articlesListItem = getArticlesListItem(properties, clientContext, articleId);
+            incrementArticlesFulfilled(articlesListItem, units);
+
+            //If the article ID is changing, reverse the updates to the old article.
+            var oldArticleId = Convert.ToString(properties.ItemEventProperties.AfterProperties[Constants.INPUT_PREVIOUS_ARTICLE_ID]);
+            if (articleId != oldArticleId)
+            {
+                var oldArticlesListItem = getArticlesListItem(properties, clientContext, oldArticleId);
+                decrementArticlesFulfilled(oldArticlesListItem, units);
+
+                //Decrement the allocation to offset the previous call to 'updateAllocationListItem'
+                var allocationId = Convert.ToString(properties.ItemEventProperties.AfterProperties[Constants.LIST_ITEM_ALLOCATION_ID]);
+                var allocationListItem = getAllocationsListItem(properties, clientContext, allocationId);
+                decrementAllocationsFulfilled(allocationListItem, units);
+            }
+
+            clientContext.ExecuteQuery();
+        }
+
+        private ListItem getAllocationsListItem(SPRemoteEventProperties properties, ClientContext clientContext, string allocationId)
         {
             ListCollection webLists = clientContext.Web.Lists;
             List allocationsList = webLists.GetByTitle(Constants.ALLOCATIONS_LIBRARY_NAME);
-
-            var allocationId = Convert.ToString(properties.ItemEventProperties.AfterProperties[Constants.LIST_ITEM_ALLOCATION_ID]);
-            var oldAllocationId = Convert.ToString(properties.ItemEventProperties.AfterProperties[Constants.INPUT_PREVIOUS_ALLOCATION_ID]);
-            var units = Convert.ToDecimal(properties.ItemEventProperties.AfterProperties[Constants.INPUT_UNIT]);
 
             var allocationQuery = new CamlQuery();
             allocationQuery.ViewXml = "<View><Query><Where><Eq><FieldRef Name='ID'/>" +
                 "<Value Type='Number'>" + allocationId + "</Value></Eq></Where></Query></View>";
             var query = allocationsList.GetItems(allocationQuery);
+
             clientContext.Load(query);
             clientContext.ExecuteQuery();
 
             if (query.Count() > 0)
             {
-                var allocationListItem = query.First();
-                var oldUnits = Convert.ToDecimal(allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_FULFILLED]);
-                var oldRemaining = Convert.ToDecimal(allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_REMAINING]);
-
-                var newRemaining = oldRemaining - units;
-                var fulfilled = oldUnits + units;
-
-                allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_REMAINING] = newRemaining;
-                allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_FULFILLED] = fulfilled;
-
-                allocationListItem.Update();
-                clientContext.ExecuteQuery();
+                return query.First();
             }
+
+            return null;
         }
 
-        private void updateArticlesListITem(ClientContext clientContext, SPRemoteEventProperties properties)
+        private ListItem getArticlesListItem(SPRemoteEventProperties properties, ClientContext clientContext, string articleId)
         {
             ListCollection webLists = clientContext.Web.Lists;
             List articlesList = webLists.GetByTitle(Constants.ARTICLES_LIBRARY_NAME);
-
-            var articleId = Convert.ToString(properties.ItemEventProperties.AfterProperties[Constants.LIST_ITEM_ARTICLE_ID]);
-            var oldArticleId = Convert.ToString(properties.ItemEventProperties.AfterProperties[Constants.INPUT_PREVIOUS_ARTICLE_ID]);
-            var units = Convert.ToDecimal(properties.ItemEventProperties.AfterProperties[Constants.INPUT_UNIT]);
 
             var articleQuery = new CamlQuery();
             articleQuery.ViewXml = "<View><Query><Where><Eq><FieldRef Name='Article_x0020_Id'/>" +
@@ -123,14 +151,65 @@ namespace FulfillmentWeb.Services
 
             if (query.Count() > 0)
             {
-                var articlesListItem = query.First();
+                return query.First();
+            }
+
+            return null;
+        }
+
+        private void incrementAllocationsFulfilled(ListItem allocationListItem, Decimal units)
+        {
+            if (allocationListItem != null)
+            {
+                var oldUnits = Convert.ToDecimal(allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_FULFILLED]);
+                var oldRemaining = Convert.ToDecimal(allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_REMAINING]);
+
+                var newRemaining = oldRemaining - units;
+                var fulfilled = oldUnits + units;
+
+                allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_REMAINING] = newRemaining;
+                allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_FULFILLED] = fulfilled;
+                allocationListItem.Update();
+            }
+        }
+
+        private void decrementAllocationsFulfilled(ListItem allocationListItem, Decimal units)
+        {
+            if (allocationListItem != null)
+            {
+                var oldUnits = Convert.ToDecimal(allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_FULFILLED]);
+                var oldRemaining = Convert.ToDecimal(allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_REMAINING]);
+
+                var newRemaining = oldRemaining + units;
+                var fulfilled = oldUnits - units;
+
+                allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_REMAINING] = newRemaining;
+                allocationListItem[Constants.LIST_ITEM_ALLOCATIONS_FULFILLED] = fulfilled;
+                allocationListItem.Update();
+            }
+        }
+
+        private void incrementArticlesFulfilled(ListItem articlesListItem, Decimal units)
+        {
+            if (articlesListItem != null)
+            {
                 var oldUnits = Convert.ToDecimal(articlesListItem[Constants.LIST_ITEM_ALLOCATIONS_FULFILLED]);
                 var fulfilled = oldUnits + units;
 
                 articlesListItem[Constants.LIST_ITEM_ALLOCATIONS_FULFILLED] = fulfilled;
-
                 articlesListItem.Update();
-                clientContext.ExecuteQuery();
+            }
+        }
+
+        private void decrementArticlesFulfilled(ListItem articlesListItem, Decimal units)
+        {
+            if (articlesListItem != null)
+            {
+                var oldUnits = Convert.ToDecimal(articlesListItem[Constants.LIST_ITEM_ALLOCATIONS_FULFILLED]);
+                var fulfilled = oldUnits - units;
+
+                articlesListItem[Constants.LIST_ITEM_ALLOCATIONS_FULFILLED] = fulfilled;
+                articlesListItem.Update();
             }
         }
     }
