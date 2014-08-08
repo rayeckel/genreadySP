592509318637a4380b25100694c3dfa506a2ceb7
Ray Eckel <rayeckel@yahoo.com>
2014-08-08 15:22:46 -0500

    Some minor adjustments made to try to fix install error

--- GRSPClassLibrary/Web/ClientAccess.cs
+++ GRSPClassLibrary/Web/ClientAccess.cs
@@ -5,7 +5,7 @@ namespace GRSPClassLibrary.Web
 {
     public class ClientAccess
     {
-        private string SITE_URL = "https://generationreadydev.sharepoint.com/sites/re";
+        private string SITE_URL = "https://generationreadydev.sharepoint.com";
         protected string sessionAccessToken { get; set; }
 
         public ClientAccess(string sessionAccessToken)
